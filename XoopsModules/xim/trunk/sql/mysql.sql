4d2c519e015ad84ecdfde9c5bea927bffdf92361
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-04-10 21:04:55 +0000

    rolling back to working rev 4683

--- XoopsModules/xim/trunk/sql/mysql.sql
+++ XoopsModules/xim/trunk/sql/mysql.sql
@@ -1,24 +1,13 @@
--- --------------------------------------------------------
--- Xoops Instant Messenger (xim)
--- Compatability (Mysql 5 & 4)
--- --------------------------------------------------------
-
--- 
--- Struktur-dump for tabellen `xoops_xim_chat`
---
 CREATE TABLE `xim_chat` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   `from` mediumint(8) unsigned NOT NULL,
   `to` mediumint(8) unsigned NOT NULL,
-  `message` text NOT NULL,
-  `sent` datetime NOT NULL default '0000-00-00 00:00:00',
-  `recd` int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `message` TEXT NOT NULL,
+  `sent` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `recd` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+)ENGINE = InnoDB;
 
--- 
--- Struktur-dump for tabellen `xoops_xim_pers_conf`
--- 
 CREATE TABLE `xim_pers_conf` (
   `id` int(1) unsigned NOT NULL auto_increment,
   `username` text NOT NULL,
