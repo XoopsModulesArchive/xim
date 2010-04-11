dc8159382d68bfeb4115f07805a9f4f73924bee9
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-04-11 11:34:54 +0000

    Danish language files

---
+++ XoopsModules/xim/trunk/language/danish/blocks.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @translation     culex.dk
+ * @specification   _LANGCODE: dk
+ * @specification   _CHARSET: UTF-8 sans Bom
+ *
+ * @Translator      culex aka Michael Albertsen (culex@culex.dk)
+ *
+ * @version         $Id$
+**/
+// added v0.4
+define("_MB_XIM_USESOUND","Lyd.:"); 
+define("_MB_XIM_NOSOUND","Ingen"); 
+define("_MB_XIM_SOUND1","Øl"); 
+define("_MB_XIM_SOUND2","Hanegal"); 
+define("_MB_XIM_SOUND3","Icq"); 
+define("_MB_XIM_SOUND4","Blob"); 
+define("_MB_XIM_SOUND5","Laser"); 
+define("_MB_XIM_SOUND6","mobil-netværk"); 
+define("_MB_XIM_SOUND7","Sværd"); 
+define("_MB_XIM_SOUND8","click"); 
+define("_MB_XIM_SOUND9","Ulv"); 
+define("_MB_XIM_SOUND10","signal"); 
+define("_MB_XIM_STATUS","Din status?");
+define("_MB_XIM_HIDDEN","Væk"); 
+define("_MB_XIM_BUSY","Optaget"); 
+define("_MB_XIM_ONLINE","Online"); 
+define("_MB_XIM_UPDATE","Opdater"); 
+
+
+?>
