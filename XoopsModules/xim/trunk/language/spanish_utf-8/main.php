830a9ba614ae10158e1ec7fe2155ad97cbf6b8bf
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-04-19 17:35:13 +0000

    Spanish utf-8 & iso for xim // Thanks to oswaldo

---
+++ XoopsModules/xim/trunk/language/spanish_utf-8/main.php
@@ -0,0 +1,32 @@
+<?php 
+
+// added v0.4
+define("_XIM_USESOUND","Sonido.:"); 
+define("_XIM_NOSOUND","Ninguno"); 
+define("_XIM_SOUND1","Cerveza"); 
+define("_XIM_SOUND2","Gallo"); 
+define("_XIM_SOUND3","Icq"); 
+define("_XIM_SOUND4","Gota"); 
+define("_XIM_SOUND5","Láser"); 
+define("_XIM_SOUND6","Red Celular"); 
+define("_XIM_SOUND7","Espada"); 
+define("_XIM_SOUND8","clic"); 
+define("_XIM_SOUND9","Lobo"); 
+define("_XIM_SOUND10","Señal"); 
+define("_XIM_STATUS","Tu Estado ?"); 
+define("_XIM_HIDDEN","Fuera"); 
+define("_XIM_BUSY","Ocupado"); 
+define("_XIM_ONLINE","En Linea"); 
+define("_XIM_UPDATE","Actualizar"); 
+
+define("_XIM_FRIENDS","Amigos");
+define("_XIM_FRIENDS_ONLINE","Amigos en Linea");  
+define("_XIM_BAR_ADMIN","Administración");
+define("_XIM_BAR_HOME","Inicio");
+define("_XIM_BAR_VIEW_PROFILE","Ver Perfil");
+define("_XIM_BAR_EDIT_PROFILE","Editar Perfil");
+define("_XIM_BAR_NOTIFICATIONS","Notificaciones");
+define("_XIM_BAR_MESSAGES","Mensajes");
+
+
+?> 
