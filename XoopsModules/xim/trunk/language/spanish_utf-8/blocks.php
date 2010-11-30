df25d930a9fd556af598dde9d6aaf45e4f56249b
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-11-30 01:51:56 +0000

    Spanish translation

--- XoopsModules/xim/trunk/language/spanish_utf-8/blocks.php
+++ XoopsModules/xim/trunk/language/spanish_utf-8/blocks.php
@@ -1,5 +1,27 @@
-﻿<?php
-
+<?php
+/**
+* You may not change or alter any portion of this comment or credits
+* of supporting developers from this source code or any supporting source code
+* which is considered copyrighted (c) material of the original comment or credit authors.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* XIM - Xoops Instant Messenger
+*
+* A one-on-one messenger written for xoops. Inspired by Anant Garg's -(anantgarg.com | inscripts.com)-
+* 2009 tutorial on jquery messenger & by the original facebook messenger and a few more. This module has
+* been adapted, written, re-written and extended heavily by Andrax & Culex.
+*
+* @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+* @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+* @package         modules
+* @subpackage      xim
+* @since           2.4.0
+* @author          Andrax - homepage.: http://guxbrasil.org & email.: lcbc@ig.com.br
+* @author          Culex  - homepage.: http://culex.dk		& email.: culex@culex.dk
+* Spanish Translation  Oswaldo Valladres - http://www.xoopsdemo.tk
+**/
 // added v0.4
 define("_MB_XIM_USESOUND","Sonido.:"); 
 define("_MB_XIM_NOSOUND","Ninguno"); 
@@ -8,16 +30,17 @@ define("_MB_XIM_SOUND2","Gallo");
 define("_MB_XIM_SOUND3","Icq"); 
 define("_MB_XIM_SOUND4","Gota"); 
 define("_MB_XIM_SOUND5","Láser"); 
-define("_MB_XIM_SOUND6","Red Célular"); 
+define("_MB_XIM_SOUND6","Red Celular"); 
 define("_MB_XIM_SOUND7","Espada"); 
 define("_MB_XIM_SOUND8","clic"); 
 define("_MB_XIM_SOUND9","Lobo"); 
 define("_MB_XIM_SOUND10","Señal"); 
-define("_MB_XIM_STATUS","Tu Estado?");
-define("_MB_XIM_HIDDEN","Fuera"); 
+define("_MB_XIM_STATUS","Su Estado?");
+define("_MB_XIM_HIDDEN","Ausente"); 
 define("_MB_XIM_BUSY","Ocupado"); 
 define("_MB_XIM_ONLINE","En Linea"); 
 define("_MB_XIM_UPDATE","Actualizar"); 
-
+define("_MB_XIM_OFFLINE", "Desconectado");
+define("_MB_XIM_ONLYYOU", "No hay amigos en línea");
 
 ?>
