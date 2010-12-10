535ebb58d70c6d45555eadd177ce604cc72fe966
kris_fr <kris_fr@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-12-10 13:47:11 +0000

    update french translation for xim module

--- modules/xim/language/french/blocks.php
+++ modules/xim/language/french/blocks.php
@@ -1,7 +1,26 @@
 <?php
-// added v0.3
-//define("_MB_XIM_BLOCK","IM"); 
-
+/**
+* You may not change or alter any portion of this comment or credits
+* of supporting developers from this source code or any supporting source code
+* which is considered copyrighted (c) material of the original comment or credit authors.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* XIM - Xoops Instant Messenger
+*
+* A one-on-one messenger written for xoops. Inspired by Anant Garg's -(anantgarg.com | inscripts.com)-
+* 2009 tutorial on jquery messenger & by the original facebook messenger and a few more. This module has
+* been adapted, written, re-written and extended heavily by Andrax & Culex.
+*
+* @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+* @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+* @package         modules
+* @subpackage      xim
+* @since           2.4.0
+* @author          Andrax - homepage.: http://guxbrasil.org & email.: lcbc@ig.com.br
+* @author          Culex  - homepage.: http://culex.dk		& email.: culex@culex.dk
+**/
 // added v0.4
 define("_MB_XIM_USESOUND","Son :"); 
 define("_MB_XIM_NOSOUND","Aucun"); 
@@ -21,6 +40,7 @@ define("_MB_XIM_BUSY","Occupé(e)");
 define("_MB_XIM_ONLINE","En ligne"); 
 define("_MB_XIM_UPDATE","Actualisation"); 
 define("_MB_XIM_OFFLINE","Hors ligne");
+define("_MB_XIM_ONLYYOU","Aucun(e) ami(e)");
 
 /**
  * @translation     Communauté Francophone des Utilisateurs de Xoops
--- modules/xim/language/french/modinfo.php
+++ modules/xim/language/french/modinfo.php
@@ -1,4 +1,27 @@
 <?php
+/**
+* You may not change or alter any portion of this comment or credits
+* of supporting developers from this source code or any supporting source code
+* which is considered copyrighted (c) material of the original comment or credit authors.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* XIM - Xoops Instant Messenger
+*
+* A one-on-one messenger written for xoops. Inspired by Anant Garg's -(anantgarg.com | inscripts.com)-
+* 2009 tutorial on jquery messenger & by the original facebook messenger and a few more. This module has
+* been adapted, written, re-written and extended heavily by Andrax & Culex.
+*
+* @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+* @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+* @package         modules
+* @subpackage      xim
+* @since           2.4.0
+* @author          Andrax - homepage.: http://guxbrasil.org & email.: lcbc@ig.com.br
+* @author          Culex  - homepage.: http://culex.dk		& email.: culex@culex.dk
+**/
+
 define("_MI_XIM_MODULE_NAME","XIM");
 define("_MI_XIM_ADMENU","Index");
 define("_MI_XIM_MODULE_DESC", "Un bloc affichant les utilisateurs en ligne. Un clic sur un nom ouvre la fenêtre de communication");
